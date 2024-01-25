af38d13e044a76bd8f02c80a1d4f969ea9a5f965
sumin <sumin510@naver.com>
2024-01-25 17:26:04 +0900

    settings.py 파일 수정 - 포트 추가

--- ssafy_pjt_django/ssafy_pjt_django/settings.py
+++ ssafy_pjt_django/ssafy_pjt_django/settings.py
@@ -109,7 +109,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://127.0.0.1:5174',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'ssafy_pjt_django.urls'
